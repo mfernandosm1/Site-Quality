75d11f8117ecbb4e74df5389ecd3529ae8128d71
mfernandosm1 <mfernando.sm1@gmail.com>
2025-10-01 18:02:22 -0300

    Ajustes

--- js/main - Copia.js
+++
@@ -1,3 +0,0 @@
-const swiper = new Swiper('.swiper', { loop: true, autoplay: { delay: 3000 }, navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev', }, });
-document.getElementById('menu-toggle').addEventListener('click', () => {document.getElementById('mobile-menu').classList.remove('-translate-x-full');});
-document.getElementById('menu-close').addEventListener('click', () => {document.getElementById('mobile-menu').classList.add('-translate-x-full');});
